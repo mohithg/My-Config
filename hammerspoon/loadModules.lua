6a1a261da33c995e4dc966549d2e5b5ff26c7ce0
mohithg <mohithgm@gmail.com>
2020-11-26 23:52:52 +0530

    Update my hammerspoon config

--- hammerspoon/loadModules.lua
+++ hammerspoon/loadModules.lua
@@ -2,7 +2,7 @@ local config = require('config')
 
 -- Clipboard Manager
 hs.loadSpoon('ClipboardTool')
-spoon.ClipboardTool.hist_size = 100
+spoon.ClipboardTool.hist_size = 1000
 spoon.ClipboardTool.show_in_menubar = false
 spoon.ClipboardTool:start()
 spoon.ClipboardTool:bindHotkeys({
@@ -16,16 +16,34 @@ local customBindings = {
 }
 spoon.Lunette:bindHotkeys(customBindings)
 
+---- Window Manager
+local hyper = config.keys.ctacms
+
+hs.loadSpoon("MiroWindowsManager")
+
+hs.window.animationDuration = 0.3
+spoon.MiroWindowsManager:bindHotkeys({
+  up = {hyper, "up"},
+  right = {hyper, "right"},
+  down = {hyper, "down"},
+  left = {hyper, "left"},
+  fullscreen = {hyper, "f"}
+})
+------- Window Manager end
+
 ---- Seal
 hs.loadSpoon('Seal')
 spoon.Seal:bindHotkeys({ show = { {"cmd"}, "space" } })
-spoon.Seal:loadPlugins({"apps", "calc", "useractions", "pasteboard"})
+spoon.Seal:loadPlugins({"apps", "calc", "useractions", "urlformats", "pasteboard", "screencapture"})
 spoon.Seal.plugins.pasteboard.historySize=4000
+spoon.Seal.plugins.urlformats:providersTable({
+	dom = { name = "DOMINO Jira", url = "https://dominodatalab.atlassian.net/browse/DOM-%s", },
+	dompr = { name = "DOMINO PRs", url = "https://github.com/cerebrotech/domino/pull/%s", },
+})
+spoon.Seal.plugins.screencapture.showPostUI=true
 spoon.Seal.plugins.useractions.actions = {
-    ["DOMINO Jira"] = { url = "https://dominodatalab.atlassian.net/browse/DOM-${query}", icon="favicon", keyword="dom" },
-    ["DMM Jira"] = { url = "https://dominodatalab.atlassian.net/browse/DMM-${query}", icon="favicon", keyword="dmm" },
-    ["DMM Git API Backend"] = { url = "https://github.com/cerebrotech/dmm-api-backend", icon="favicon", keyword="git-dmm-backend" },
-    ["DMM Git Crux"] = { url = "https://github.com/cerebrotech/dmm-crux", icon="favicon", keyword="git-dmm-crux" },
+    ["AMG"] = { url = "https://dominodatalab.atlassian.net/secure/RapidBoard.jspa?projectKey=DOM&rapidView=223", icon="favicon", keyword="amg" },
+    ["Domino my prs"] = { url = "https://github.com/cerebrotech/domino/pulls", icon="favicon", keyword="prs" },
 }
 spoon.Seal:refreshAllCommands()
 spoon.Seal:start()
